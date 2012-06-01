0a05c11e408d1901f977ec75441430c11ebb9e37
Peter Desnoyers <pjd@ccs.neu.edu>
2012-06-01 13:07:28 -0400

    add windowed greedy

--- build-dist.sh
+++ build-dist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-VERSION=0.1
+VERSION=0.2
 DIR=ftlsim-$VERSION
 rm -rf $DIR
 mkdir -p $DIR
@@ -9,7 +9,7 @@ mkdir -p $DIR
 MISC='README.txt setup.py'
 SRC='ftlsim.c ftlsim.i ftlsim.h getaddr.c getaddr.i getaddr.h 
      lambertw.c lambertw.i'
-SCRIPTS='low-level.py greedy-high.py lru-high.py 2hc.py 3hc.py two-pool.py'
+SCRIPTS='low-level.py greedy-high.py lru-high.py 2hc.py 3hc.py two-pool.py windowed-greedy.py'
 
 cp $MISC $SRC $SCRIPTS $DIR/
 tar cvfz $DIR.tar.gz $DIR
