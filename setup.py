5d80dd88d68e24e7e07d2c6b12fba187efe8ec21
Peter Desnoyers <pjd@ccs.neu.edu>
2012-06-01 13:25:17 -0400

    added GPL

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
+#
+# Copyright 2012 Peter Desnoyers
+# This file is part of ftlsim.
+# ftlsim is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version. 
+#
+# ftlsim is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+# You should have received a copy of the GNU General Public License
+# along with ftlsim. If not, see http://www.gnu.org/licenses/. 
+#
 """
-setup.py file for simulator
+setup.py file for ftlsim
 """
 
 from distutils.core import setup, Extension
@@ -11,7 +26,7 @@ adr_module = Extension('_getaddr', sources=['getaddr.i', 'getaddr.c'] )
 w_module = Extension('_lambertw', sources=['lambertw.i', 'lambertw.c'] )
 
 setup (name = 'newsim',
-       version = '0.1',
+       version = '0.2',
        author      = "Peter Desnoyers",
        author_email = "pjd@ccs.neu.edu",
        description = """Modular high-speed FTL simulator""",
