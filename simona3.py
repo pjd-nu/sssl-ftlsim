819cae90280d7591508a23f5f5e7a8a21e06fd9a
Peter Desnoyers <pjd@ccs.neu.edu>
2012-12-04 18:14:16 -0500

    close enough - error about 0.3%

--- simona3.py
+++ simona3.py
@@ -34,6 +34,9 @@
 
 import getaddr
 import ftlsim
+import copy
+
+from pydbgr.api import debug
 
 # Total blocks and user-visible blocks, based on parameters above
 #
@@ -41,6 +44,10 @@ T_plane = 2048
 U_plane = 2048 - 163
 minfree = 20                            # blocks per plane
 minfree_elmt = 164
+
+#T_plane = 512
+#U_plane = 512 - 40
+#minfree_elmt = 41
 
 T = T_plane * 8 * 8
 U = U_plane * 8 * 8
@@ -72,51 +79,53 @@ freemap = [ [None] * blks_per_elmt for i in range(nelements) ]
 next_free = [0] * nelements
 blks_free = [0] * nelements
 
-freelists = []
+initblks = []
+physblks = []
 for i in range(nelements):
     list = [ftlsim.segment(Np) for j in range(blks_per_elmt)]
     for s,j in zip(list,range(blks_per_elmt)):
 	s.thisown = False
 	s.elem = i
 	s.blkno = j
-    freelists.append(list)
-
-import copy
-physblks = [ copy.copy(map) for map in freemap ]
+    initblks.append(list)
+    physblks.append(copy.copy(list))
 
 def getblk(elmt):
     global freemap, next_free, blks_free
     assert blks_free[elmt] > 0
     _map = freemap[elmt]
     i = next_free[elmt] % blks_per_elmt
-    #j = i
     while True:                 # fails for full map, but much faster
 	if _map[i] is not None:  # than range()
 	    tmp = _map[i]
 	    _map[i] = None
 	    blks_free[elmt] -= 1
 	    next_free[elmt] = i
-	    assert tmp.elem == elmt
 	    return tmp
         i = (i+1) % blks_per_elmt
     return None
+
+print_putblk = False
 
 def putblk(elmt, blk):
     global freemap, blks_free
     assert blk.elem == elmt
     _map = freemap[elmt]
     idx = (blk.blkno % nplanes) * blks_per_plane + blk.blkno / nplanes
+    #if print_putblk:
+    #    print "putblk:", blk.blkno, elmt, idx
     assert _map[idx] is None
     _map[idx] = blk
     blks_free[elmt] += 1
-    
+
 for i in range(nelements):
-    elements[i].add_segment(freelists[i].pop(0))
+    elements[i].add_segment(initblks[i].pop(0))
 
 intwrites = 0
 extwrites = 0
 
 verbose = True
+verbose = False
 
 # MSR algorithm - write at active page in active block, then go to next free block.
 # blocks numbered 0..N-1 in element, if full_stripe then (block mod N_elements) = plane
@@ -137,9 +146,11 @@ def int_write(ftl, elem, lba):
         print "write: %d %d %d %d" % (lba/nelements,
                                      e.frontier.blkno * (Np+1) + e.i,
                                      e.frontier.blkno % nplanes, elem)
+            #    if intwrites >= 99998:
+            #        debug()
+        
     e.frontier.write(e.i, lba)
     e.i += 1
-    #print e.frontier.blkno*64 + e.i
     if e.i >= Np:
         e.add_segment(getblk(elem))
 
@@ -152,32 +163,42 @@ def host_write(lba):
     # writes are striped by LBA between elements
     #
     elem = lba % nelements
-    int_write(ftl, elem, lba)
 
-    if blks_free[elem] < minfree_elmt:
+    if verbose:
+        print "req:", lba*8, 8
+    
+    if blks_free[elem] < minfree_elmt-1:
 	pool = elements[elem]
-	while blks_free[elem] <= minfree_elmt:
+	while blks_free[elem] <= minfree_elmt-1:
 	    b = pool.remove_segment()
+            if verbose:
+                print "clean2:", elem, b.blkno % 8, b.blkno
+            #if b.blkno == 1575:
+            #debug()
 	    for i in range(Np):
 		a = b.page(i)
 		if a != -1:
 		    int_write(ftl, elem, a)
 	    putblk(elem, b)
 
+    int_write(ftl, elem, lba)
+
+
 # initialize by writing every LBA once. inlined from int_write...
 #
-#print 'writing', U*Np
 for lba in range(U*Np):
     elem = lba % nelements
     e = elements[elem]
     e.frontier.write(e.i, lba)
     e.i += 1
     if e.i >= Np:
-        e.add_segment(freelists[elem].pop(0))
+        e.add_segment(initblks[elem].pop(0))
 
 for e in range(nelements):
-    for b in freelists[e]:
+    for b in initblks[e]:
 	putblk(e, b)
+
+print_putblk = True
 
 def printall():
     for i in range(nelements):
@@ -212,13 +233,14 @@ def printall():
 
 import sys
 file = sys.argv[1]
+
 sum_e = 0
 sum_i = 0
 src = getaddr.trace(file)
 
 # Now write a full volume's worth of random data.
 #
-i,j = 0,0
+i,j,a = 0,0,0
 while not src.eof:
     extwrites,intwrites = 0,0
     i=0
@@ -227,6 +249,9 @@ while not src.eof:
         host_write(a)
         a = src.handle.next()
         i += 1
+    if a != -1:
+        host_write(a)
+
     if extwrites > 0 and not verbose:
         print extwrites, intwrites, (1.0*intwrites)/extwrites
     sum_e += extwrites
@@ -235,4 +260,5 @@ while not src.eof:
 if not verbose:
     print (1.0*sum_i)/sum_e
 
-#printall()
+if verbose:
+    printall()
