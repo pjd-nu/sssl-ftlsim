4496b4f1aa945f600a25880115c4ccbb8d7b70b4
Peter Desnoyers <pjd@ccs.neu.edu>
2014-09-01 10:46:59 -0400

    better parsing

--- opt.py
+++ opt.py
@@ -1,15 +1,22 @@
-import sys
+import sys, re
 
-def parse(file):
-    d = dict()
-    fp = open(file, 'r')
-    for line in fp:
-        if line[0] == '#':
-            continue
-        tmp = line.split()
-        if len(tmp) != 3 or tmp[1] != '=':
-            print 'error:', line
-            sys.exit()
-        d[tmp[0]] = tmp[2]
-    fp.close()
-    return d
+class parse:
+    def __init__(self, file):
+        fp = open(file, 'r')
+        for line in fp:
+            if line[0] == '#' or line[0] == '\n':
+                continue
+            tmp = line.split()
+            if len(tmp) != 3 or tmp[1] != '=':
+                print 'error:', line
+                sys.exit()
+            name,_,val = tmp
+            if val.isdigit():
+                val = int(tmp[2])
+            elif re.match('[0-9]*\.[0-9]*', val):
+                val = float(tmp[2])
+            setattr(self, tmp[0], val)
+        fp.close()
+
+    def has(self, name):
+        return hasattr(self, name)
